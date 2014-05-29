c4bf1800a1e7d68e0167d5093c180f08adde5677
Nathan Woods <big.nate.w@gmail.com>
2014-05-28 23:40:03 -0600

    Cleaning Gruntfile + 404 handling

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,44 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-        shell: {
-            jekyllBuild: {
-                command: 'jekyll build'
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 8080,
-                    base: '_site',
-	                middleware: function (connect, options) {
-                        return [function (req, res) {
-                            var filename = (req.url.substr(-1) == '/') ? (req.url + 'index.html') : req.url;
-                            if (!grunt.file.exists(options.base + filename)) filename = '/404.html';
-                            res.end(grunt.file.read(options.base + filename));
-                        }];
-	                },
-                }
-            }
-        },
-        watch: {
-          livereload: {
-            files: [
-                '**/*', 'skin/**/*', '!_site/**/*', '!**/node_modules/**/*'
-            ],
-            tasks: ['shell:jekyllBuild'],
-            options: {
-              livereload: 1337,
-            },
+	grunt.initConfig({
+		shell: {
+			jekyllBuild: {
+				command: 'jekyll build'
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 8080,
+					base: '_site',
+					middleware: function (connect, options) {
+						var middlewares = [];
+						middlewares.push(function (req, res, next) {
+							var filename = (req.url.substr(-1) == '/') ? (req.url + 'index.html') : req.url;
+							if (grunt.file.exists(options.base + filename)) return next();
+							res.end(grunt.file.read(options.base + '/404.html'));
+						});
+						middlewares.push(connect.static(options.base));
+						return middlewares;
+					},
+				}
+			}
+		},
+		watch: {
+		  livereload: {
+			files: [
+				'**/*', 'skin/**/*', '!_site/**/*', '!**/node_modules/**/*'
+			],
+			tasks: ['shell:jekyllBuild'],
+			options: {
+			  livereload: 1337,
+			},
 
-          },
-        }
-    });
+		  },
+		}
+	});
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.registerTask('default', ['shell', 'connect', 'watch'])
-}
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.registerTask('default', ['shell', 'connect', 'watch'])
+};
